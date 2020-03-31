c93c57e68fd0d9441b39bae491bc785158b7f3b3
Justin Pinkney <justinpinkney@gmail.com>
2020-03-31 11:20:48 +0100

    fix bug #6 in proposeRegions

---
+++ test/+tests/ProposeRegionsTest.m
@@ -0,0 +1,47 @@
+classdef ProposeRegionsTest < matlab.unittest.TestCase
+    % Test propose regions function
+    
+    %  Copyright 2020 The MathWorks, Inc.
+    
+    properties (Constant)
+        Image = imread("visionteam.jpg");
+        Weights = load(fullfile(mtcnnRoot, "weights", "pnet.mat"));
+    end
+    
+    methods (Test)
+        function testOutputs(test)
+            scale = 2;
+            conf = 0.5;
+            
+            [box, score] = mtcnn.proposeRegions(test.Image, scale, conf, test.Weights);
+            
+            test.verifyOutputs(box, score);
+        end
+        
+        function test1DActivations(test)
+            % Test for bug #6 (1xn activations causes proposeRegions to
+            % fail)
+            cropped = imcrop(test.Image, [300, 42, 65, 38]);
+            scale = 3;
+            conf = 0.5;
+            
+            [box, score] = mtcnn.proposeRegions(cropped, scale, conf, test.Weights);
+            
+            test.verifyOutputs(box, score);
+        end
+    end
+    
+    methods
+        function verifyOutputs(test, box, score)
+            % helper to check expected outputs of proposeRegions
+            test.verifyEqual(size(box, 2), 4, ...
+                "first output should be nx4 bounding box list");
+            test.verifyEqual(size(score, 2), 1, ...
+                "second output should be nx1 face probabilities");
+            test.verifyEqual(size(box, 1), size(score, 1), ...
+                "should be 1 box for each probability");
+        end
+    end
+    
+end
+    
