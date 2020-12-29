56b7209d373b21af8adc1ee5eca6d068d3539766
andycampbell <andycampbell@andys-mbp>
2020-12-29 18:41:48 -0500

    Real time

--- perfresults2json.m
+++ perfresults2json.m
@@ -8,19 +8,14 @@ context.host_name = results(1).Samples.Host(1);
 %c.cpu_scaling_enabled = false;
 %c.caches = NaN;
 
-for thisResultIdx = numel(results):-1:1
-    thisResult = results(thisResultIdx);
-    b = struct;
-    b.name = thisResult.Name;
-    samples =  height(thisResult.Samples);
-    b.iterations = samples;
-    b.real_time = median(thisResult.Samples.MeasuredTime);
-    b.cpu_time = b.real_time;
-    b.time_unit = "s";
-    b.threads = 1;
-    
-    benchmarks(thisResultIdx) = b;
-end
+benchmarks = arrayfun(@(r) struct(...
+    "name", r.Name, ...
+    "iterations", 1, ...
+    "real_time", median(r.Samples.MeasuredTime), ...
+    "cpu_time", min(r.Samples.MeasuredTime), ...
+    "time_unit", "s", ...
+    "threads", 1), ...
+    results);
 
 s.context = context;
 s.benchmarks = benchmarks;
