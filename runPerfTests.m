a15defc9b5ee9c13f6339df7380b44ce59815d25
andycampbell <andycampbell@andys-mbp>
2020-12-29 13:06:25 -0500

    Fix benchmark output location

--- runPerfTests.m
+++ runPerfTests.m
@@ -5,6 +5,6 @@ results = runperf("Superclass", "matlab.perftest.TestCase");
 json = perfresults2json(results);
 [~,~] = mkdir('output');
 
-fid = fopen("output/benchmark-result.json","w","n","UTF-8");
+fid = fopen("benchmark-data.json","w","n","UTF-8");
 cl = onCleanup(@() fclose(fid));
 fprintf(fid,"%s",json);
