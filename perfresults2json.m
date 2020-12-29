2acc655c6f5bd9da9c9f0536c49053e9cf82a935
andycampbell <andycampbell@andys-mbp>
2020-12-29 14:55:51 -0500

    adjsut

--- perfresults2json.m
+++ perfresults2json.m
@@ -8,19 +8,18 @@ c.host_name = results(1).Samples.Host(1);
 %c.cpu_scaling_enabled = false;
 %c.caches = NaN;
 
-samples = vertcat(results.Samples);
-
-for thisSampleIdx = height(samples):-1:1
-    thisSample = samples(thisSampleIdx,:);
+for thisResultIdx = numel(results):-1:1
+    thisResult = results(thisResultIdx);
     b = struct;
-    b.name = thisSample.Name;
-    b.iterations = 1;
-    b.real_time = round(thisSample.MeasuredTime*1e9);
+    b.name = thisResult.Name;
+    samples =  height(thisResult.Samples);
+    b.iterations = samples;
+    b.real_time = round(sum(thisResult.Samples.MeasuredTime)/samples*1e9);
     b.cpu_time = b.real_time;
     b.time_unit = "ns";
     b.threads = 1;
     
-    benchmarks(thisSampleIdx) = b;
+    benchmarks(thisResultIdx) = b;
 end
 
 s.context = c;
--- runPerfTests.m
+++ runPerfTests.m
@@ -9,6 +9,4 @@ fid = fopen("benchmark-data.json","w","n","UTF-8");
 cl = onCleanup(@() fclose(fid));
 fprintf(fid,"%s",json);
 
-which benchmark-data.json
-
 disp(fileread("benchmark-data.json"))
