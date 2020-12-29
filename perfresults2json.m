260221b68968c9b99c27a8299152af77b4e307f4
andycampbell <andycampbell@andys-mbp>
2020-12-29 15:05:55 -0500

    Add other publish

--- perfresults2json.m
+++ perfresults2json.m
@@ -1,7 +1,7 @@
 function json = perfresults2json(results)
 
-c.date = datetime('now');
-c.host_name = results(1).Samples.Host(1);
+context.date = datetime('now');
+context.host_name = results(1).Samples.Host(1);
 %c.executable = 'MATLAB';
 %c.num_cpus = NaN;
 %c.mhz_per_cpu = NaN;
@@ -22,6 +22,6 @@ for thisResultIdx = numel(results):-1:1
     benchmarks(thisResultIdx) = b;
 end
 
-s.context = c;
+s.context = context;
 s.benchmarks = benchmarks;
 json = jsonencode(s);
